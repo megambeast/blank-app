549b10495bcd2bcee1ed693a4b7b43cf9e7a26b3
megambeast <michaellieu8@gmail.com>
2024-09-23 12:19:06 +0000

    Version 1 - 23.09.2024

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My Beginner App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import pandas as pd
+import numpy as np
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+df = pd.DataFrame(np.random.randint(0, 100, size=(3, 3)), columns=['Column1', 'Column2', 'Column3'])
+
+st.table(df)
+
