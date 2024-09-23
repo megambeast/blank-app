edbb0119dc4ffbaa757b80cf258971b52f56fd19
megambeast <michaellieu8@gmail.com>
2024-09-23 12:38:58 +0000

    Version 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,28 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from requests import get, post
+import json
+import base64
+import datetime
 
 st.title("🎈 My Beginner App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). I'm using this app as a playground for all my python projects right now."
 )
 
-import pandas as pd
-import numpy as np
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
@@ -14,7 +30,9 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 }))
 
-df = pd.DataFrame(np.random.randint(0, 100, size=(3, 3)), columns=['Column1', 'Column2', 'Column3'])
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-st.table(df)
-
+    chart_data
